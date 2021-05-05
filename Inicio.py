15ceb76c6d6d8cda4473b8261b3d1b2e3535b1f2
ZoiliSanchez <38706142+ZoilaSanchez@users.noreply.github.com>
2021-05-04 18:24:09 -0600

    Primeras prubas

--- Inicio.py
+++ Inicio.py
@@ -1,56 +1,12 @@
 import os
-import cv2
+import predict as proceso
 
-# FUNCIONES----------------------------
-def show_image(text, img):
-    cv2.imshow(text, img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-def tomarfoto():
-    # camara
-    captura = cv2.VideoCapture(0)
-    while (True):
-        # Caputramos la foto
-        return_Varlor, image = captura.read();
-        # visualizar
-        if return_Varlor:
-            cv2.imshow('visor', image)
-        # salir
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            cv2.imwrite('opencv1.png', image)
-            del (captura)
-            break
-    return 'opencv1.PNG'
-
-def recorte(carguardar, imgaleer):
-    directory = carguardar
-    img = cv2.imread(imgaleer, 1)
-    dim = (760, 760)
-    img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
-    os.chdir(directory)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    blurred = cv2.GaussianBlur(gray, (5, 5), 8)
-    blurred = cv2.GaussianBlur(blurred, (7, 7), 0)
-    canny = cv2.Canny(blurred, 100, 300, apertureSize=3)  # apertura debe sesta entre 3 y 7
-    contours, _ = cv2.findContours(canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-    cnts = sorted(contours, key=cv2.contourArea, reverse=True)
-
-    i = 1
-    for c in cnts:
-        x, y, w, h = cv2.boundingRect(c)
-        recortada = img[y:y + h, x:x + w]
-        show_image('imagen no:' + str(i), recortada)
-        cv2.imwrite('img' + str(i) + '.PNG', recortada)
-        i += 1
-    return i-1
-# FUNCIONES----------------------------
 def menu():
     os.system('cls')  # NOTA para windows tienes que cambiar clear por cls
     print("Selecciona una opción")
     print("\t1 - Multiples imagenes")
     print("\t2 - Una imagen")
-    print("\t3 - Reducir imagen")
-    print("\t4 - salir")
+    print("\t3 - salir")
 
 
 while True:
@@ -60,16 +16,12 @@ while True:
     if opcionMenu == "1":
         print("")
         input("Multiples imagenes...\n")
-        x=recorte("C:\\Users\\Lopez\\Documents\\GitHub\\IA\\guardar",tomarfoto())
-        print("Realizado con extito ",x)
-        print("\n\n\n")
+       # proceso.recorte(carguardar=,imgaleer=,cargardatos=,cargarmodelo=,imgaen2=)
     elif opcionMenu == "2":
         print("")
         input("Una imagen....\n")
+        #proceso.recortar(path=,directorio=)
     elif opcionMenu == "3":
-        print("")
-        input("Has pulsado la opción 3...\npulsa una tecla para continuar")
-    elif opcionMenu == "4":
         break
     else:
         print("")
--- predict.py
+++ predict.py
@@ -4,15 +4,6 @@ import os
 from tensorflow import keras
 import cv2
 
-# Se carga el nombre de las carpetas donde se encuentran las imágenes, correspondiendo a las etiquetas que utiliza
-# el modelo guardado
-images_path = 'C:/Users/ACER/Desktop/Copia/IA/Pruebas opencv/Aguacates'
-mylist = os.listdir(images_path)
-# Se carga el modelo previamente entrenado (En este caso es un modelo con 50 épocas y con data augmentation)
-keras_model_path = "C:/Users/ACER/Desktop/Copia/IA/Modelo2"
-restored_keras_model = tf.keras.models.load_model(keras_model_path)
-
-
 # Función para devolver el tiempo aproximado en el que se podrá consumir el aguacate.
 def print_time_left(etiqueta):
     if etiqueta == 2:
@@ -32,9 +23,9 @@ def show_image(text, img):
 
 # Se utiliza la misma función para manejar la entrada de datos de entrenamiento, principalmente para recortar el fondo
 # y que la detección sea más acertadas.
-def recortar(path):
+def recortar(path,directorio):
     image = cv2.imread(path, 1)
-    directory = 'D:/URL/Séptimo ciclo 2021/Inteligencia artificial/Phyton/predict_avocado'
+    directory = directorio
     os.chdir(directory)
     width = 800
     height = 800
@@ -59,30 +50,79 @@ def recortar(path):
         show_image('imagen', recortada)
         cv2.imwrite('img_after_cv2.jpg', recortada)
         break
+    return 'img_after_cv2.jpg'
+# FUNCIONES----------------------------
 
+def tomarfoto():
+    # camara
+    captura = cv2.VideoCapture(0)
+    while (True):
+        # Caputramos la foto
+        return_Varlor, image = captura.read();
+        # visualizar
+        if return_Varlor:
+            cv2.imshow('visor', image)
+        # salir
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            cv2.imwrite('opencv1.png', image)
+            del (captura)
+            break
+    return 'opencv1.PNG'
 
-# Se carga la imagen y se muestra el proceso para quitar el fondo.
-img_path = 'C:/Users/ACER/Desktop/Pruebas/avocado7.jpg'
-recortar(img_path)
-img_height = 180
-img_width = 180
-# Se carga la imágen a analizar con un preprocesamiento que la estandariza al tamaño con el que se entrenó la CNN
-img = keras.preprocessing.image.load_img(
-    'D:/URL/Séptimo ciclo 2021/Inteligencia artificial/Phyton/predict_avocado/img_after_cv2.jpg',
-    target_size=(img_height, img_width)
-)
-# Se utiliza este método para pasar la imágen a un array 3d que la red neuronal pueda interpretar
-img_array = keras.preprocessing.image.img_to_array(img)
-img_array = tf.expand_dims(img_array, 0)  # Create a batch
-# Se utiliza el método .predict(img) para que abtenga las probabilidades de pertenecer a cada clase, y almacenamos
-# únicamente la más alta.
-predictions = restored_keras_model.predict(img_array)
-score = tf.nn.softmax(predictions[0])
-# Devuelve al usuario la clase con mayor probabilidad.
-print("--------  RESULTADOS   --------")
-print(
-    "Este aguacate pertenece a la etiqueta {} con un {:.2f}% de certeza."
-        .format(mylist[np.argmax(score)], 100 * np.max(score))
-)
-print(print_time_left(np.argmax(score)))
+def recorte(carguardar, imgaleer,cargardatos,cargarmodelo,imgaen2):
+    directory = carguardar
+    img = cv2.imread(imgaleer, 1)
+    dim = (760, 760)
+    img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+    os.chdir(directory)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 8)
+    blurred = cv2.GaussianBlur(blurred, (7, 7), 0)
+    canny = cv2.Canny(blurred, 100, 300, apertureSize=3)  # apertura debe sesta entre 3 y 7
+    contours, _ = cv2.findContours(canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    cnts = sorted(contours, key=cv2.contourArea, reverse=True)
+
+    i = 1
+    for c in cnts:
+        x, y, w, h = cv2.boundingRect(c)
+        recortada = img[y:y + h, x:x + w]
+        show_image('imagen no:' + str(i), recortada)
+        cv2.imwrite('img' + str(i) + '.PNG', recortada)
+        procesoreconocer(cargardatos, cargarmodelo, 'img'+str(i)+".PNG", imgaen2)
+        i += 1
+    return i-1
+# FUNCIONES----------------------------
+def procesoreconocer(cargardatos,cargarmodelo,imagendemuestra,imgaen2):
+    # Se carga el nombre de las carpetas donde se encuentran las imágenes, correspondiendo a las etiquetas que utiliza
+    # el modelo guardado
+    images_path = cargardatos
+    mylist = os.listdir(images_path)
+    # Se carga el modelo previamente entrenado (En este caso es un modelo con 50 épocas y con data augmentation)
+    keras_model_path = cargarmodelo
+    restored_keras_model = tf.keras.models.load_model(keras_model_path)
+
+    # Se carga la imagen y se muestra el proceso para quitar el fondo.
+    img_path = imagendemuestra
+    recortar(img_path)
+    img_height = 180
+    img_width = 180
+    # Se carga la imágen a analizar con un preprocesamiento que la estandariza al tamaño con el que se entrenó la CNN
+    img = keras.preprocessing.image.load_img(
+        imgaen2,
+        target_size=(img_height, img_width)
+    )
+    # Se utiliza este método para pasar la imágen a un array 3d que la red neuronal pueda interpretar
+    img_array = keras.preprocessing.image.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0)  # Create a batch
+    # Se utiliza el método .predict(img) para que abtenga las probabilidades de pertenecer a cada clase, y almacenamos
+    # únicamente la más alta.
+    predictions = restored_keras_model.predict(img_array)
+    score = tf.nn.softmax(predictions[0])
+    # Devuelve al usuario la clase con mayor probabilidad.
+    print("--------  RESULTADOS   --------")
+    print(
+        "Este aguacate pertenece a la etiqueta {} con un {:.2f}% de certeza."
+            .format(mylist[np.argmax(score)], 100 * np.max(score))
+        )
+    print(print_time_left(np.argmax(score)))
 
